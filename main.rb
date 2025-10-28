d3013f8d0113e2022c69661209e7bda3d8706deb
Shuflduf <shuflduf@gmail.com>
2025-10-28 06:03:27 -0600

    process function

--- main.rb
+++ main.rb
@@ -10,3 +10,17 @@ canvas[:height] = height
 ctx = canvas.getContext("2d")
 ctx[:fillStyle] = "white"
 ctx.fillRect(10, 10, width - 20, height - 20)
+
+$last_frame = 0
+def process(new_frame)
+  delta = new_frame - $last_frame
+  $last_frame = new_frame
+  puts delta
+
+  # things go here i think
+  
+  JS.global.requestAnimationFrame{|ts| process(ts.to_i) }
+end
+
+process(16)
+
