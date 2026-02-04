62bf95e61d2d30a1a5e219c73c0a70c77292058b
Shuflduf <shuflduf@gmail.com>
2026-02-04 12:54:47 -0700

    cant fuckin

--- main.rb
+++ main.rb
@@ -1,27 +1,68 @@
-require "js"
+# frozen_string_literal: true
 
-width = JS.global[:window][:outerWidth].to_i
-height = JS.global[:window][:outerHeight].to_i
+require 'js'
 
-canvas = JS.global[:document].getElementById('game')
-canvas[:width] = width
-canvas[:height] = height
+# drawing to js canvas
+class Canvas
+  def initialize(board)
+    @board = board
+    @canvas = JS.global[:document].getElementById('game')
+    @ctx = @canvas.getContext('2d')
+    @last_frame = 0
 
-ctx = canvas.getContext('2d')
+    process(16)
+  end
 
-ctx[:fillStyle] = 'red'
-ctx.fillRect(0, 0, width, height)
-ctx[:fillStyle] = 'blue'
-ctx.fillRect(10, 10, width - 80, height - 80)
+  def process(new_frame)
+    update_canvas
 
-$last_frame = 0
+    delta = new_frame - @last_frame
+    @last_frame = new_frame
 
-def process(new_frame)
-  delta = new_frame - $last_frame
-  $last_frame = new_frame
-  # puts delta
+    JS.global.requestAnimationFrame { |ts| process(ts.to_i) }
+  end
 
-  JS.global.requestAnimationFrame { |ts| process(ts.to_i) }
+  def update_canvas
+    @width = JS.global[:window][:innerWidth].to_i
+    @height = JS.global[:window][:innerHeight].to_i
+    @canvas[:width] = width
+    @canvas[:height] = height
+    # puts @width
+
+    draw
+  end
+
+  def draw
+    @ctx[:fillStyle] = 'red'
+    @ctx.fillRect(0, 0, @width, @height)
+    @ctx[:fillStyle] = 'blue'
+    @ctx.fillRect(10, 10, @width - 80, @height - 80)
+
+    @board.draw_outline!(@ctx)
+  end
+
+  attr_reader :width, :height
+  attr_accessor :ctx
 end
 
-process(16)
+# game board
+class Board
+  @height = 20
+  @width = 10
+
+  def initialize
+    @board = Array.new(@width, 0) { Array.new(@height, 0) }
+  end
+
+  def draw_outline!(ctx)
+    # tile_size
+    (0..@height).each do |y|
+      @ctx[:fillStyle] = 'black'
+      ctx.fillRect(3, 3, 50, 50)
+    end
+  end
+end
+
+board = Board.new
+_c = Canvas.new(board)
+# puts c.width
