eb7aa2aaf5f6e2035c41956e12f68b3402b4d23a
Shuflduf <shuflduf@gmail.com>
2026-02-04 11:14:21 -0700

    coming back to this proj

--- main.rb
+++ main.rb
@@ -1,26 +1,27 @@
 require "js"
 
-width =  JS.global[:window][:innerWidth].to_i
-height = JS.global[:window][:innerHeight].to_i
+width = JS.global[:window][:outerWidth].to_i
+height = JS.global[:window][:outerHeight].to_i
 
-canvas = JS.global[:document].getElementById("game")
+canvas = JS.global[:document].getElementById('game')
 canvas[:width] = width
 canvas[:height] = height
 
-ctx = canvas.getContext("2d")
-ctx[:fillStyle] = "white"
-ctx.fillRect(10, 10, width - 20, height - 20)
+ctx = canvas.getContext('2d')
+
+ctx[:fillStyle] = 'red'
+ctx.fillRect(0, 0, width, height)
+ctx[:fillStyle] = 'blue'
+ctx.fillRect(10, 10, width - 80, height - 80)
 
 $last_frame = 0
+
 def process(new_frame)
   delta = new_frame - $last_frame
   $last_frame = new_frame
-  puts delta
+  # puts delta
 
-  # things go here i think
-  
-  JS.global.requestAnimationFrame{|ts| process(ts.to_i) }
+  JS.global.requestAnimationFrame { |ts| process(ts.to_i) }
 end
 
 process(16)
-
